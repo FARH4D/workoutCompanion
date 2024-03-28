e72c7e5af973b31a688a0d6dc3f20aea15239bb5
FARH4D <FarhadAhmed24@hotmail.co.uk>
2024-03-28 18:04:23 +0000

    Completed Home Page

--- app/src/main/java/com/example/workoutcompanion/homeActivity.java
+++ app/src/main/java/com/example/workoutcompanion/homeActivity.java
@@ -1,13 +1,14 @@
 package com.example.workoutcompanion;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class homeActivity extends AppCompatActivity {
 
@@ -16,7 +17,15 @@ public class homeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
+        TextView homeButton = findViewById(R.id.navHome);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homeIntent = new Intent(homeActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+                overridePendingTransition(0, 0);
+            }
+        });
     }
     @Override
     public void onBackPressed() {
